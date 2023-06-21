c86f33602d127cd522ec3f28c1c5c70ef09bf91d
Bill Mill <bill@billmill.org>
2023-06-20 21:06:44 -0400

    don't use global app instance
    discussion ongoing: https://github.com/obsidianmd/obsidian-releases/pull/2049

--- main.ts
+++ main.ts
@@ -1,31 +1,36 @@
-import { Plugin, TFile } from "obsidian";
+import { App, Plugin, TFile } from "obsidian";
 import { ulid } from "ulid";
 
-async function addID(f: TFile): Promise<void> {
-    const key = "id";
-    console.log("checking file {f}");
-    if (!app.metadataCache.getFileCache(f)?.frontmatter?.[key]) {
-        console.log("adding id {f}");
-        await app.fileManager.processFrontMatter(f, (data) => {
-            data[key] = ulid();
-        });
-    }
+function addID(app: App): (f: TFile) => Promise<void> {
+    return async function (f: TFile): Promise<void> {
+        const key = "id";
+        if (!app.metadataCache.getFileCache(f)?.frontmatter?.[key]) {
+            await app.fileManager.processFrontMatter(f, (data) => {
+                data[key] = ulid();
+            });
+        }
+    };
 }
 
-function addIDsToAllNotes() {
-    app.vault.getMarkdownFiles().forEach((f) => addID(f));
+function addIDsToAllNotes(app: App) {
+    const _addID = addID(app);
+    return function () {
+        app.vault.getMarkdownFiles().forEach((f) => _addID(f));
+    };
 }
 
 export default class IDPlugin extends Plugin {
     async onload() {
         // Called when a file has been indexed, and its (updated) cache is now
         // available.
-        this.registerEvent(this.app.metadataCache.on("changed", addID));
+        this.registerEvent(
+            this.app.metadataCache.on("changed", addID(this.app))
+        );
 
         this.addCommand({
             id: "add-ids-to-all-notes",
             name: "Add an ID to all notes",
-            callback: addIDsToAllNotes,
+            callback: addIDsToAllNotes(this.app),
         });
     }
 }
